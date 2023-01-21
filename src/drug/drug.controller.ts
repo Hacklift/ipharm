e03ac2240f7b3d0f280536a45e81996b31f9ffa1
Obinna Nnenanya <obinna.nnenanya@primarybid.com>
2023-01-21 15:59:29 +0100

    Add more tests

--- src/drug/drug.controller.ts
+++ src/drug/drug.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { FilterQuery } from 'mongoose';
 import { InsertDrugInput } from '../models';
 import { Drug } from '../schema';
 import { DrugService } from './drug.service';
@@ -13,8 +14,10 @@ export class DrugController {
   }
 
   @Get("drugs")
-  async findDrugs(): Promise<Drug[]> {
-    return this.drugService.findAll();
+  async findDrugs(
+    drugFilterQuery?: FilterQuery<Drug>
+  ): Promise<Drug[]> {
+    return this.drugService.findAll(drugFilterQuery);
   }
 
   /**
